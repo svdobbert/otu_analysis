bdfca6d4be0a3aa7ba93f8bf3bef09f47217305b
svdobbert <svenjadobbert@gmail.com>
2025-02-12 15:37:51 +0100

    fix: version conflict

--- src/dependencies.jl
+++ src/dependencies.jl
@@ -4,6 +4,7 @@ using Pkg
 
 Pkg.activate(".")
 
+Pkg.add("Gadfly")               # a system for plotting and visualization 
 Pkg.add("Arrow")                # Arrow storage and file format
 Pkg.add("CategoricalArrays")    # similar to the factor type in R
 Pkg.add("CSVFiles")             # read/write CSV and similar formats
@@ -18,7 +19,6 @@ Pkg.add("Dates")                # provides types for working with dates
 Pkg.add("Statistics")           # basic statistics functionality.
 Pkg.add("Missings")             # Convenience functions for working with missing values in Julia
 Pkg.add("LinearAlgebra")        # common and useful linear algebra operations 
-Pkg.add("Gadfly")               # a system for plotting and visualization 
 Pkg.add("Compose")              # a declarative vector graphics system written in Julia
 Pkg.add("ColorSchemes")         # a set of pre-defined ColorSchemes
-Pkg.add("Jchemo")               # Chemometrics and machine learning on high-dimensional data       
+Pkg.add("Jchemo")               # Chemometrics and machine learning on high-dimensional data
