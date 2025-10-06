da2878bd34973f63a17ba342f3e725b297a84790
svdobbert <svenjadobbert@gmail.com>
2025-10-06 11:48:34 +0200

    feat: refactor pca

--- src/pca-sites.jl
+++ src/pca-sites.jl
@@ -14,53 +14,39 @@ end
 type = "DNA"
 time = "01d"
 
-function pca(type::String, time::String)
+function pca(type::String)
     """
     Calculates a PCA and plots the results using PlotlyJS.
 
     Parameters:
     - `type::String`: The type of data to be used for the PCA. Can be "cDNA" or "DNA".
- 	- `time::String`: The time point for which the PCA should be calculated. E.g. "01d", "07d", etc.
 
     Returns:
     - A 3d PCA-Plot.
     """
-    filename_at = "data/$(type)results/$(type)_AT_$(time)_postprocessing_R_explVar.xlsx"
-    filename_st = "data/$(type)results/$(type)_ST_$(time)_postprocessing_R_explVar.xlsx"
-    filename_sm = "data/$(type)results/$(type)_SM_$(time)_postprocessing_R_explVar.xlsx"
+    filename = "data/$(type)_mean_sorted_for_PCA.xlsx"
     filename_taxa = "data/$(type)_taxa.csv"
     filename_colors = "colours_PCA_order.xlsx"
 
-    df_at = DataFrame(XLSX.readtable(filename_at, "Sheet1", infer_eltypes=true))
-    df_at.x = round.(df_at.x, digits=1)
-    df_at = combine(groupby(df_at, :x), names(df_at) .=> mean; keepkeys=false)
-
-    df_st = DataFrame(XLSX.readtable(filename_st, "Sheet1", infer_eltypes=true))
-    df_st.x = round.(df_st.x, digits=1)
-    df_st = combine(groupby(df_st, :x), names(df_st) .=> mean; keepkeys=false)
-
-    df_sm = DataFrame(XLSX.readtable(filename_sm, "Sheet1", infer_eltypes=true))
-    df_sm.x = round.(df_sm.x, digits=2)
-    df_sm = combine(groupby(df_sm, :x), names(df_sm) .=> mean; keepkeys=false)
-
-    df_at.env_var = fill("AT", nrow(df_at))
-    df_st.env_var = fill("ST", nrow(df_st))
-    df_sm.env_var = fill("SM", nrow(df_sm))
-	rename!(df_at, names(df_at)[1] => :x)
-	rename!(df_st, names(df_st)[1] => :x)
-    rename!(df_sm, names(df_sm)[1] => :x)
-    df = vcat(df_at, df_st, df_sm)
+    df = DataFrame(XLSX.readtable(filename, "Sheet1", infer_eltypes=true))
     # df = drop_cols_with_missing(df, 0.8)
 
     # df_clean = dropmissing(df)
     df_clean = coalesce.(df, 0)
-    features = Symbol.(string.(round.(df_clean.x, digits = 2)) .* "_" .* df_clean.env_var)
-    env_var = Symbol.(df_clean.env_var)
+    features = Symbol.(string.(df_clean.x))
     df_clean = df_clean[:, Not([1, ncol(df_clean)])]
     df_clean = DataFrame([[names(df_clean)]; collect.(eachrow(df_clean))], [:column; Symbol.(axes(df_clean, 1))])
+    rename!(df_clean, names(df_clean)[1] => :OTU_ID)
 
     df_taxa = CSV.read(filename_taxa, DataFrame)
+    df_taxa = leftjoin(df_clean, df_taxa, on = :OTU_ID)
     df_clean = hcat(df_taxa[:, Symbol("Order")], df_clean)
+
+    for c in names(df_clean[!, 3:end])
+        if eltype(df_clean[!, c]) <: AbstractString
+            df_clean[!, c] = parse.(Float64, df_clean[!, c])
+        end
+    end
 
     # load and fit PCA
     PCA = @load PCA pkg = "MultivariateStats"
@@ -77,15 +63,9 @@ function pca(type::String, time::String)
 	
 	rename!(df_loadings, features)
 
-	CSV.write("./pca/pca_loadings_$(type)_$(time)_explained_variance.csv", df_loadings)
+	CSV.write("./pca/pca_loadings_$(type)_sites.csv", df_loadings)
 
     # plot PCA
-    unique_groups = unique(env_var)
-    palette = ["#b4b4b4", "#646464", "#000000"]
-    group_colors = Dict(g => color for (g, color) in zip(unique_groups, palette))
-
-    colors_env = [group_colors[env_var[i]] for i in 1:length(features)]
-
     df_colors = DataFrame(XLSX.readtable(filename_colors, "Tabelle1", infer_eltypes=true))
     df_colors = rightjoin(df_colors, components, on = :order)[!, [:colour]]
     colors_points = df_colors.colour
@@ -116,12 +96,12 @@ function pca(type::String, time::String)
             y=[0, loadings[2, i]],
             z=[0, loadings[3, i]],
             mode="lines+text",
-            line=attr(color=colors_env[i], width=4),
+            line=attr(color="#646464", width=2),
             text=[nothing, features[i]],
             textposition="top center",
             textfont=attr(
-                size=14, 
-                color=colors_env[i]), 
+                size=12, 
+                color="#646464"), 
             name=features[i],
             showlegend=false
         )
@@ -131,7 +111,7 @@ function pca(type::String, time::String)
     p = PlotlyJS.plot(
         [scatter_points; arrow_traces...],
         Layout(
-            title="$(type) $(time) explained variance, total variance: $(round(total_var, digits=3))",
+            title="$(type) sites, total variance: $(round(total_var, digits=3))",
             scene=attr(
                 xaxis=attr(title="PC1"),
                 yaxis=attr(title="PC2"),
@@ -140,10 +120,10 @@ function pca(type::String, time::String)
         )
     )
 
-    filename = "pca_$(type)_$(time)_explained_variance.html"
+    filename = "pca_$(type)_sites.html"
     savefig(p, "./pca/$(filename)")
 end
 
-pca(type, time)
+pca(type)
 
 
